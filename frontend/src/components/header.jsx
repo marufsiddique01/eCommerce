ab01aa6906e50c8498db62f8bb87ea22548638e8
marufsiddique01 <marufsiddique71@yahoo.com>
2021-06-12 23:02:41 +0600

    favicon in header

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const header = () => {
   return (
     <>
-      <Navbar bg='light' expand='lg'>
-        <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='mr-auto'>
-            <Nav.Link href='#home'>Home</Nav.Link>
-            <Nav.Link href='#link'>Link</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+      <Navbar bg='dark' expand='lg' variant='dark' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='#home'>Online Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link href='#home'>
+                <i className='fas fa-shopping-cart'></i>
+                &nbsp; Cart
+              </Nav.Link>
+              <Nav.Link href='#link'>
+                <i className='fas fa-user'></i>
+                &nbsp; Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </>
   );
