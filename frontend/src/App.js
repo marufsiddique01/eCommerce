d89cb167a43331a277bd2cf3cefe28619a4216de
marufsiddique01 <marufsiddique71@yahoo.com>
2021-06-13 16:32:04 +0600

    products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import { Container } from 'react-bootstrap';
 import Footer from './components/footer';
 import Header from './components/header';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
     <>
       <Header />
+
       <main>
         <Container>
           <h1>Ecommerce app</h1>
+          <HomeScreen />
         </Container>
       </main>
 
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Products from '../products';
+import { Row, Col } from 'react-bootstrap';
+import ProductScreen from './ProductScreen';
 
 const HomeScreen = () => {
-  return <div></div>;
+  return (
+    <>
+      <Row>
+        {Products.map((product) => (
+          <Col>
+            <ProductScreen Products={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default HomeScreen;
