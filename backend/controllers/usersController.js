9e5237013dd0de81ad346f0efd20f38fb7663f24
marufsiddique01 <marufsiddique71@yahoo.com>
2021-07-01 01:48:54 +0600

    Auth middleware for protected routes

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -19,4 +19,19 @@ const authController = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { authController };
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User Not Found');
+  }
+});
+
+module.exports = { authController, getUserProfile };
--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const { protect } = require('../middlewares/authMiddleware');
 
-const { authController } = require('../controllers/usersController');
+const {
+  authController,
+  getUserProfile,
+} = require('../controllers/usersController');
 
 const router = express.Router();
 
 //post email and password
 
 router.post('/login', authController);
+
+//get user profile or private route
+
+router.route('/profile').get(protect, getUserProfile);
 
 module.exports = router;
